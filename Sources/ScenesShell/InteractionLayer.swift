5f2eb51d4b03353aacb26ab0befa281876fd7584
Camden Thomson <camden.thomson@student.allenisd.org>
2021-03-31 13:23:47 -0500

    Initial commit

---
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -0,0 +1,17 @@
+import Scenes
+import Scenes3D
+
+/*
+ This class is responsible for the interaction Layer.
+ Internally, it maintains the RenderableEntities for this layer.
+ */
+
+class InteractionLayer : Layer {
+    
+    init() {
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Interaction")
+        
+        // We insert our RenderableEntities in the constructor
+    }
+}
